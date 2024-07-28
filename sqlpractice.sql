154656180c26889e9227e0a65f3f625d37fe41b8
raghavarahul <raghavak235@gmail.com>
2024-07-27 21:45:27 -0400

    Adding More

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -1542,5 +1542,99 @@ COUNT(DISTINCT user_id) as active_users
 from activity
 where activity_date BETWEEN DATE_ADD('2019-07-27', INTERVAL -29 DAY) AND '2019-07-27'
 group by activity_date
+===========================================================================================================================================================================================================================z
 
-==========================================================================================================================================================================================================
+Ques:
+NPV Queries
+
+Table: NPV
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| year          | int     |
+| npv           | int     |
++---------------+---------+
+(id, year) is the primary key (combination of columns with unique values) of this table.
+The table has information about the id and the year of each inventory and the corresponding net present value.
+
+
+Table: Queries
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| year          | int     |
++---------------+---------+
+(id, year) is the primary key (combination of columns with unique values) of this table.
+The table has information about the id and the year of each inventory query.
+
+
+Write a solution to find the npv of each query of the Queries table.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+NPV table:
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2018   | 100    |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 11   | 2020   | 99     |
+| 7    | 2019   | 0      |
++------+--------+--------+
+Queries table:
++------+--------+
+| id   | year   |
++------+--------+
+| 1    | 2019   |
+| 2    | 2008   |
+| 3    | 2009   |
+| 7    | 2018   |
+| 7    | 2019   |
+| 7    | 2020   |
+| 13   | 2019   |
++------+--------+
+Output:
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 7    | 2018   | 0      |
+| 7    | 2019   | 0      |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
++------+--------+--------+
+Explanation:
+The npv value of (7, 2018) is not present in the NPV table, we consider it 0.
+The npv values of all other queries can be found in the NPV table.
+
+ChatGPT:
+The IFNULL function in SQL is used to return a specified value if the expression is NULL. Otherwise, it returns the expression. It is commonly used to handle NULL values in a dataset.
+
+In the context of your query, IFNULL(n.npv, 0) means that if n.npv is NULL, it will return 0 instead. If n.npv is not NULL, it will return the value of n.
+
+Sol:
+# Write your MySQL query statement below# Write your MySQL query statement below
+SELECT q.id, q.year, ifnull(n.npv,0) as npv
+FROM npv n
+RIGHT JOIN queries q
+ON n.id = q.id AND n.year = q.year
+order by q.id
+===========================================================================================================================================================================================================================z
+
