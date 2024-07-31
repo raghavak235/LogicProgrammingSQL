de34133603ed62ec3bd71e06828bb3e4681a1028
raghavarahul <raghavak235@gmail.com>
2024-07-31 18:43:28 -0400

    Adding More

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -1709,3 +1709,161 @@ END;
 
 ============================================================================================================================================================================================================================================
 
+Ques:
+Project Employees II
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key (combination of columns with unique values) of this table.
+employee_id is a foreign key (reference column) to Employee table.
+Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+
+
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key (column with unique values) of this table.
+Each row of this table contains information about one employee.
+
+
+Write a solution to report all the projects that have the most employees.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+Output:
++-------------+
+| project_id  |
++-------------+
+| 1           |
++-------------+
+Explanation: The first project has 3 employees while the second one has 2.
+
+Sol:
+# Write your MySQL query statement below
+
+with cte as(
+select project_id,count(*) as cnt  from project
+group by  project_id)
+select project_id from cte
+where cnt = (select max(cnt) from cte)
+
+============================================================================================================================================================================================================================================
+
+Que:
+Sales Analysis I
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table can have repeated rows.
+product_id is a foreign key (reference column) to the Product table.
+Each row of this table contains some information about one sale.
+
+
+Write a solution that reports the best seller by total sales price, If there is a tie, report them all.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Product table:
++------------+--------------+------------+
+| product_id | product_name | unit_price |
++------------+--------------+------------+
+| 1          | S8           | 1000       |
+| 2          | G4           | 800        |
+| 3          | iPhone       | 1400       |
++------------+--------------+------------+
+Sales table:
++-----------+------------+----------+------------+----------+-------+
+| seller_id | product_id | buyer_id | sale_date  | quantity | price |
++-----------+------------+----------+------------+----------+-------+
+| 1         | 1          | 1        | 2019-01-21 | 2        | 2000  |
+| 1         | 2          | 2        | 2019-02-17 | 1        | 800   |
+| 2         | 2          | 3        | 2019-06-02 | 1        | 800   |
+| 3         | 3          | 4        | 2019-05-13 | 2        | 2800  |
++-----------+------------+----------+------------+----------+-------+
+Output:
++-------------+
+| seller_id   |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: Both sellers with id 1 and 3 sold products with the most total price of 2800.
+
+Sol:
+
+WITH jt AS (
+  SELECT s.seller_id, SUM(s.price) AS price_amt
+  FROM product d
+  INNER JOIN Sales s ON d.product_id = s.product_id
+  GROUP BY s.seller_id
+)
+SELECT jt.seller_id
+FROM jt
+WHERE jt.price_amt = (SELECT MAX(price_amt) FROM jt);
+
+=============================================================================================================================================================================================================================================
