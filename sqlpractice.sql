e8bf528082743d5d8d969e852a63028dea5a04fb
raghavarahul <raghavak235@gmail.com>
2024-08-02 17:58:56 -0400

    Adding More

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -1867,3 +1867,156 @@ FROM jt
 WHERE jt.price_amt = (SELECT MAX(price_amt) FROM jt);
 
 =============================================================================================================================================================================================================================================
+
+
+Ques:
+Sales Analysis II
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table might have repeated rows.
+product_id is a foreign key (reference column) to the Product table.
+buyer_id is never NULL.
+sale_date is never NULL.
+Each row of this table contains some information about one sale.
+
+
+Write a solution to report the buyers who have bought S8 but not iPhone. Note that S8 and iPhone are products presented in the Product table.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Product table:
++------------+--------------+------------+
+| product_id | product_name | unit_price |
++------------+--------------+------------+
+| 1          | S8           | 1000       |
+| 2          | G4           | 800        |
+| 3          | iPhone       | 1400       |
++------------+--------------+------------+
+Sales table:
++-----------+------------+----------+------------+----------+-------+
+| seller_id | product_id | buyer_id | sale_date  | quantity | price |
++-----------+------------+----------+------------+----------+-------+
+| 1         | 1          | 1        | 2019-01-21 | 2        | 2000  |
+| 1         | 2          | 2        | 2019-02-17 | 1        | 800   |
+| 2         | 1          | 3        | 2019-06-02 | 1        | 800   |
+| 3         | 3          | 3        | 2019-05-13 | 2        | 2800  |
++-----------+------------+----------+------------+----------+-------+
+Output:
++-------------+
+| buyer_id    |
++-------------+
+| 1           |
++-------------+
+Explanation: The buyer with id 1 bought an S8 but did not buy an iPhone. The buyer with id 3 bought both.
+
+Sol:
+
+SELECT buyer_id
+FROM Product as p
+JOIN Sales as s ON p.product_id=s.product_id
+group by buyer_id
+having SUM(p.product_name = 'S8') >0 and SUM(p.product_name = 'iphone')=0x
+
+=============================================================================================================================================================================================================================================
+
+Ques:
+
+Reported Posts
+Table: Actions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| post_id       | int     |
+| action_date   | date    |
+| action        | enum    |
+| extra         | varchar |
++---------------+---------+
+This table may have duplicate rows.
+The action column is an ENUM (category) type of ('view', 'like', 'reaction', 'comment', 'report', 'share').
+The extra column has optional information about the action, such as a reason for the report or a type of reaction.
+extra is never NULL.
+
+
+Write a solution to report the number of posts reported yesterday for each report reason. Assume today is 2019-07-05.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Actions table:
++---------+---------+-------------+--------+--------+
+| user_id | post_id | action_date | action | extra  |
++---------+---------+-------------+--------+--------+
+| 1       | 1       | 2019-07-01  | view   | null   |
+| 1       | 1       | 2019-07-01  | like   | null   |
+| 1       | 1       | 2019-07-01  | share  | null   |
+| 2       | 4       | 2019-07-04  | view   | null   |
+| 2       | 4       | 2019-07-04  | report | spam   |
+| 3       | 4       | 2019-07-04  | view   | null   |
+| 3       | 4       | 2019-07-04  | report | spam   |
+| 4       | 3       | 2019-07-02  | view   | null   |
+| 4       | 3       | 2019-07-02  | report | spam   |
+| 5       | 2       | 2019-07-04  | view   | null   |
+| 5       | 2       | 2019-07-04  | report | racism |
+| 5       | 5       | 2019-07-04  | view   | null   |
+| 5       | 5       | 2019-07-04  | report | racism |
++---------+---------+-------------+--------+--------+
+Output:
++---------------+--------------+
+| report_reason | report_count |
++---------------+--------------+
+| spam          | 1            |
+| racism        | 2            |
++---------------+--------------+
+Explanation: Note that we only care about report reasons with non-zero number of reports.
+
+Sol:
+
+# Write your MySQL query statement below
+select  extra as report_reason, count(distinct post_id) as report_count from actions
+where extra is not null and
+action_date ='2019-07-04'
+AND action = 'report'
+group by extra
+
+=============================================================================================================================================================================================================================================
+
+
+
+
+
