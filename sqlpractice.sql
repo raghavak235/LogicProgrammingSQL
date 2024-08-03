5aa590da27a9fd83a62ee4478822719611c8ea79
raghavarahul <raghavak235@gmail.com>
2024-08-03 14:29:15 -0400

    Adding More

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -2016,7 +2016,207 @@ group by extra
 
 =============================================================================================================================================================================================================================================
 
+Ques:
+
+Weather Type in Each Country
+Table: Countries
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| country_id    | int     |
+| country_name  | varchar |
++---------------+---------+
+country_id is the primary key (column with unique values) for this table.
+Each row of this table contains the ID and the name of one country.
+
+
+Table: Weather
+
++---------------+------+
+| Column Name   | Type |
++---------------+------+
+| country_id    | int  |
+| weather_state | int  |
+| day           | date |
++---------------+------+
+(country_id, day) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the weather state in a country for one day.
+
+
+Write a solution to find the type of weather in each country for November 2019.
+
+The type of weather is:
+
+Cold if the average weather_state is less than or equal 15,
+Hot if the average weather_state is greater than or equal to 25, and
+Warm otherwise.
+Return the result table in any order.
+
+The result format is in the following example.
 
 
 
+Example 1:
+
+Input:
+Countries table:
++------------+--------------+
+| country_id | country_name |
++------------+--------------+
+| 2          | USA          |
+| 3          | Australia    |
+| 7          | Peru         |
+| 5          | China        |
+| 8          | Morocco      |
+| 9          | Spain        |
++------------+--------------+
+Weather table:
++------------+---------------+------------+
+| country_id | weather_state | day        |
++------------+---------------+------------+
+| 2          | 15            | 2019-11-01 |
+| 2          | 12            | 2019-10-28 |
+| 2          | 12            | 2019-10-27 |
+| 3          | -2            | 2019-11-10 |
+| 3          | 0             | 2019-11-11 |
+| 3          | 3             | 2019-11-12 |
+| 5          | 16            | 2019-11-07 |
+| 5          | 18            | 2019-11-09 |
+| 5          | 21            | 2019-11-23 |
+| 7          | 25            | 2019-11-28 |
+| 7          | 22            | 2019-12-01 |
+| 7          | 20            | 2019-12-02 |
+| 8          | 25            | 2019-11-05 |
+| 8          | 27            | 2019-11-15 |
+| 8          | 31            | 2019-11-25 |
+| 9          | 7             | 2019-10-23 |
+| 9          | 3             | 2019-12-23 |
++------------+---------------+------------+
+Output:
++--------------+--------------+
+| country_name | weather_type |
++--------------+--------------+
+| USA          | Cold         |
+| Australia    | Cold         |
+| Peru         | Hot          |
+| Morocco      | Hot          |
+| China        | Warm         |
++--------------+--------------+
+Explanation:
+Average weather_state in USA in November is (15) / 1 = 15 so weather type is Cold.
+Average weather_state in Austraila in November is (-2 + 0 + 3) / 3 = 0.333 so weather type is Cold.
+Average weather_state in Peru in November is (25) / 1 = 25 so the weather type is Hot.
+Average weather_state in China in November is (16 + 18 + 21) / 3 = 18.333 so weather type is Warm.
+Average weather_state in Morocco in November is (25 + 27 + 31) / 3 = 27.667 so weather type is Hot.
+We know nothing about the average weather_state in Spain in November so we do not include it in the result table.
+
+Sol:
+
+select c.country_name,
+CASE
+    WHEN avg(w.weather_state) <= 15 then 'Cold'
+    WHEN avg(w.weather_state) >=25 THEN 'Hot'
+    ELSE 'Warm'
+END as weather_type
+from countries c
+inner join weather w
+on c.country_id = w.country_id
+where day between '2019-11-01' and '2019-11-30'
+group by c.country_id
+
+
+===============================================================================================================================================================================================================================================
+
+
+Ques:
+Ads Performance
+Easy
+Topics
+Companies
+SQL Schema
+Pandas Schema
+Table: Ads
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| ad_id         | int     |
+| user_id       | int     |
+| action        | enum    |
++---------------+---------+
+(ad_id, user_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the ID of an Ad, the ID of a user, and the action taken by this user regarding this Ad.
+The action column is an ENUM (category) type of ('Clicked', 'Viewed', 'Ignored').
+
+
+A company is running Ads and wants to calculate the performance of each Ad.
+
+Performance of the Ad is measured using Click-Through Rate (CTR) where:
+
+
+Write a solution to find the ctr of each Ad. Round ctr to two decimal points.
+
+Return the result table ordered by ctr in descending order and by ad_id in ascending order in case of a tie.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Ads table:
++-------+---------+---------+
+| ad_id | user_id | action  |
++-------+---------+---------+
+| 1     | 1       | Clicked |
+| 2     | 2       | Clicked |
+| 3     | 3       | Viewed  |
+| 5     | 5       | Ignored |
+| 1     | 7       | Ignored |
+| 2     | 7       | Viewed  |
+| 3     | 5       | Clicked |
+| 1     | 4       | Viewed  |
+| 2     | 11      | Viewed  |
+| 1     | 2       | Clicked |
++-------+---------+---------+
+Output:
++-------+-------+
+| ad_id | ctr   |
++-------+-------+
+| 1     | 66.67 |
+| 3     | 50.00 |
+| 2     | 33.33 |
+| 5     | 0.00  |
++-------+-------+
+Explanation:
+for ad_id = 1, ctr = (2/(2+1)) * 100 = 66.67
+for ad_id = 2, ctr = (1/(1+2)) * 100 = 33.33
+for ad_id = 3, ctr = (1/(1+1)) * 100 = 50.00
+for ad_id = 5, ctr = 0.00, Note that ad_id = 5 has no clicks or views.
+Note that we do not care about Ignored Ads.
+
+Sol:
+
+SELECT
+    ad_id,
+    CASE
+        WHEN COUNT(CASE WHEN action='Clicked' THEN 1 END) + COUNT(CASE WHEN action='Viewed' THEN 1 END) = 0
+        THEN 0
+        ELSE ROUND(
+            COUNT(CASE WHEN action='Clicked' THEN 1 END) /
+            (COUNT(CASE WHEN action='Clicked' THEN 1 END) + COUNT(CASE WHEN action='Viewed' THEN 1 END)) * 100,
+            2
+        )
+    END AS ctr
+FROM
+    Ads
+GROUP BY
+    ad_id
+ORDER BY
+    ctr DESC,
+    ad_id ASC;
+
+===============================================================================================================================================================================================================================================
 
