fd3c4e48af805dddd9d984a366c1bb15fa1eeffb
raghavarahul <raghavak235@gmail.com>
2024-08-23 07:28:24 -0400

    Adding More

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -2476,3 +2476,171 @@ ON p.sub_p = c.cmt
 order by p.sub_p;
 
 ===============================================================================================================================================================================================================================================
+
+Que:
+Unique Orders and Customers Per Month
+
+Table: Orders
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| order_id      | int     |
+| order_date    | date    |
+| customer_id   | int     |
+| invoice       | int     |
++---------------+---------+
+order_id is the column with unique values for this table.
+This table contains information about the orders made by customer_id.
+
+
+Write a solution to find the number of unique orders and the number of unique customers with invoices > $20 for each different month.
+
+Return the result table sorted in any order.
+
+The result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Orders table:
++----------+------------+-------------+------------+
+| order_id | order_date | customer_id | invoice    |
++----------+------------+-------------+------------+
+| 1        | 2020-09-15 | 1           | 30         |
+| 2        | 2020-09-17 | 2           | 90         |
+| 3        | 2020-10-06 | 3           | 20         |
+| 4        | 2020-10-20 | 3           | 21         |
+| 5        | 2020-11-10 | 1           | 10         |
+| 6        | 2020-11-21 | 2           | 15         |
+| 7        | 2020-12-01 | 4           | 55         |
+| 8        | 2020-12-03 | 4           | 77         |
+| 9        | 2021-01-07 | 3           | 31         |
+| 10       | 2021-01-15 | 2           | 20         |
++----------+------------+-------------+------------+
+Output:
++---------+-------------+----------------+
+| month   | order_count | customer_count |
++---------+-------------+----------------+
+| 2020-09 | 2           | 2              |
+| 2020-10 | 1           | 1              |
+| 2020-12 | 2           | 1              |
+| 2021-01 | 1           | 1              |
++---------+-------------+----------------+
+Explanation:
+In September 2020 we have two orders from 2 different customers with invoices > $20.
+In October 2020 we have two orders from 1 customer, and only one of the two orders has invoice > $20.
+In November 2020 we have two orders from 2 different customers but invoices < $20, so we don't include that month.
+In December 2020 we have two orders from 1 customer both with invoices > $20.
+In January 2021 we have two orders from 2 different customers, but only one of them with invoice > $20.
+
+
+ChatGPT:
+SELECT DATE_FORMAT('2020-09-15', '%Y-%m') AS YearMonth;
+
+Sol:
+# Write your MySQL query statement below
+select
+DATE_FORMAT(order_date,'%Y-%m') as month,
+COUNT(order_id) as order_count,
+count(DISTINCT customer_id) as customer_count
+from orders
+where  invoice > 20
+group by month
+
+===============================================================================================================================================================================================================================================
+
+Que:
+Warehouse Manager
+Table: Warehouse
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| name         | varchar |
+| product_id   | int     |
+| units        | int     |
++--------------+---------+
+(name, product_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the information of the products in each warehouse.
+
+
+Table: Products
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| product_name  | varchar |
+| Width         | int     |
+| Length        | int     |
+| Height        | int     |
++---------------+---------+
+product_id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the product dimensions (Width, Lenght, and Height) in feets of each product.
+
+
+Write a solution to report the number of cubic feet of volume the inventory occupies in each warehouse.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Warehouse table:
++------------+--------------+-------------+
+| name       | product_id   | units       |
++------------+--------------+-------------+
+| LCHouse1   | 1            | 1           |
+| LCHouse1   | 2            | 10          |
+| LCHouse1   | 3            | 5           |
+| LCHouse2   | 1            | 2           |
+| LCHouse2   | 2            | 2           |
+| LCHouse3   | 4            | 1           |
++------------+--------------+-------------+
+Products table:
++------------+--------------+------------+----------+-----------+
+| product_id | product_name | Width      | Length   | Height    |
++------------+--------------+------------+----------+-----------+
+| 1          | LC-TV        | 5          | 50       | 40        |
+| 2          | LC-KeyChain  | 5          | 5        | 5         |
+| 3          | LC-Phone     | 2          | 10       | 10        |
+| 4          | LC-T-Shirt   | 4          | 10       | 20        |
++------------+--------------+------------+----------+-----------+
+Output:
++----------------+------------+
+| warehouse_name | volume     |
++----------------+------------+
+| LCHouse1       | 12250      |
+| LCHouse2       | 20250      |
+| LCHouse3       | 800        |
++----------------+------------+
+Explanation:
+Volume of product_id = 1 (LC-TV), 5x50x40 = 10000
+Volume of product_id = 2 (LC-KeyChain), 5x5x5 = 125
+Volume of product_id = 3 (LC-Phone), 2x10x10 = 200
+Volume of product_id = 4 (LC-T-Shirt), 4x10x20 = 800
+LCHouse1: 1 unit of LC-TV + 10 units of LC-KeyChain + 5 units of LC-Phone.
+          Total volume: 1*10000 + 10*125  + 5*200 = 12250 cubic feet
+LCHouse2: 2 units of LC-TV + 2 units of LC-KeyChain.
+          Total volume: 2*10000 + 2*125 = 20250 cubic feet
+LCHouse3: 1 unit of LC-T-Shirt.
+          Total volume: 1*800 = 800 cubic feet.
+
+Sol:
+# Write your MySQL query statement below
+select
+w.name as warehouse_name,
+sum(w.units*p.width* p.length* p.height)as volume
+from warehouse w
+inner join products p
+on w.product_id=p.product_id
+GROUP BY w.name
+
+===============================================================================================================================================================================================================================================
